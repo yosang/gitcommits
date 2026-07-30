9a7263cb486f02b34ab37425b24c43b5524f328e
Yosmel Chiang <yosmelchiang@gmail.com>
2026-07-30 20:11:09 +0200

    refactor: simplify commit message display and log retrieval

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ async function diff() {
         })
         const commitMessage = response.choices[0].message.content
         const commit = await git.commit(commitMessage);
-        console.log(`Commit created, push it, or change it with --amend: `, commitMessage);
+        await git.log(['--oneline'])
+        console.log(`Commit created, push it, or change it with --amend`);
     } catch(err) {
         console.log("Operation failed", err.message)
     }
